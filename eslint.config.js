a4fe437045a0467ff4f591df982491e72ee9fb25
Cylix Lee <cylix.lee@outlook.com>
2025-07-21 23:24:21 +0800

    project: Introduce ESLint Stylistic.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { stylistic }, extends: ['stylistic/recommended'] },
+])
+
+// With ESLint extension installed in VSCodium, just set some settings in "[javascript]"."editor.codeActionsOnSave" to
+// get the functionality of import organizing and sorting. No need to introduce some dependencies like
+// `eslint-plugin-import`.
