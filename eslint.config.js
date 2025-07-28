a8fcdccd21b97811dcedf4cd95a832ea523d9776
Cylix Lee <cylix.lee@outlook.com>
2025-07-28 10:42:44 +0800

    project: Sync ESLint.

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,12 @@ export default defineConfig([
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs}'], plugins: { stylistic }, extends: ['stylistic/recommended'] },
+  {
+    rules: {
+      'no-unused-vars': ['warn'],
+      '@stylistic/brace-style': ['error', '1tbs'],
+    },
+  },
 ])
 
 // With ESLint extension installed in VSCodium, just set some settings in "[javascript]"."editor.codeActionsOnSave" to
